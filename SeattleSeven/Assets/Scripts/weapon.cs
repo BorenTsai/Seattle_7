04305bc199cbd8f6d553ec9a3cd5d7936155f3c6
Madi <mhight@gmail.com>
2018-09-16 10:16:16 -0700

    Audio Fixes

--- SeattleSeven/Assets/Scripts/weapon.cs
+++ SeattleSeven/Assets/Scripts/weapon.cs
@@ -6,10 +6,12 @@ public class weapon : MonoBehaviour {
 
     public string enemy_tag;
     public weaponManager weapon_manager;
+    private AudioSource slash;
 
     private void Start()
     {
         weapon_manager = GameObject.Find("WeaponManager").GetComponent<weaponManager>();
+        slash = gameObject.GetComponent<AudioSource>();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -17,6 +19,7 @@ public class weapon : MonoBehaviour {
         if (collision.gameObject.CompareTag(enemy_tag))
         {
             weapon_manager.hit();
+            slash.PlayOneShot(slash.clip);
         }
         else
         {
--- SeattleSeven/Assets/Scripts/weaponManager.cs
+++ SeattleSeven/Assets/Scripts/weaponManager.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class weaponManager : MonoBehaviour {
 
-    public UnityEngine.UI.Text messagesTextBox;
-    public UnityEngine.UI.Text healthTextBox;
+    public UnityEngine.UI.Text MessageBox;
+    public UnityEngine.UI.Text HealthBox;
     private int health = 100;
     public float m_StartingHealth = 100;
     public Color m_FullHealthColor;
     public Color m_ZeroHealthColor;
+
+    private AudioSource speaker;
+    public AudioClip cheer_clip; 
+    public AudioClip dying_clip;
 
     public void Start()
     {
@@ -19,24 +23,37 @@ public class weaponManager : MonoBehaviour {
 
     public void win()
     {
-        healthTextBox.text = string.Empty;
-        messagesTextBox.text = "You Win!";
+        HealthBox.text = string.Empty;
+        MessageBox.text = "You Win!";
+        speaker= gameObject.GetComponent<AudioSource>();
+        speaker.PlayOneShot(cheer_clip);
     }
 
     public void damage()
     {
         health -= 10;
-        healthTextBox.text = health.ToString();
+        HealthBox.text = health.ToString();
+        if (health <= 0) {
+            death();
+        }
+    }
+
+    public void death()
+    {
+        health = 0;
+        HealthBox.text = "You have died. You have let down the human race. Gods win.";
+        speaker.PlayOneShot(dying_clip);
     }
 
     public void hit()
     {
-        messagesTextBox.text = "Good Hit!";
+        MessageBox.text = "Good Hit!";
+        damage();
     }
 
     public void wrong()
     {
-        messagesTextBox.text = "Wrong Weapon";
+        MessageBox.text = "Wrong Weapon";
     }
 
 }
